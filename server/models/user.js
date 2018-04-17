a20fc003cb080e2594984cbdafbb69116ffba650
Michael Caveney <dylanesque27@gmail.com>
2018-04-17 19:52:28 -0400

    Add files via upload

---
+++ server/models/user.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  email: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+});
+
+
+module.exports = { User };
